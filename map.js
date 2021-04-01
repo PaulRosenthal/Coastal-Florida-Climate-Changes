6c2799c7b58edb4ce60c6d1a30e0f1c482d55a16
Paul Rosenthal <Paul.H.Rosenthal@iCloud.com>
2021-04-01 00:29:56 -0500

    Create map.js

---
+++ map.js
@@ -0,0 +1,22 @@
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/TileLayer"
+], function (Map, MapView, TileLayer) {
+
+  var layer = new TileLayer({
+    url: "https://www.coast.noaa.gov/arcgis/rest/services/dc_slr/slr_9ft/MapServer",
+  });
+
+  var map = new Map({
+    basemap: "gray",
+    layers: [layer]
+  });
+
+  var view = new MapView({
+    container: "myMap",
+    map: map,
+    center: [-123, 44],
+    zoom: 13
+  });
+});
