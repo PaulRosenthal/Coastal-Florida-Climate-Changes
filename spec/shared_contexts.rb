53a6c668fa4ccef2a5e37614a2dd9bdd58cc5d2c
Paul Rosenthal <Paul.H.Rosenthal@iCloud.com>
2022-02-06 17:23:28 -0600

    Update Test Suite

--- spec/shared_contexts.rb
+++ spec/shared_contexts.rb
@@ -1,5 +1,5 @@
 # Every page that is accessible by users should contain the word "Florida" within it.
-RSpec.shared_examples_for 'All Pages' do
+RSpec.shared_examples_for 'Page' do
   it 'renders correctly' do
     expect(page.status_code).to be(200)
   end
@@ -8,5 +8,11 @@ RSpec.shared_examples_for 'All Pages' do
   end
   it 'contains the date the website was last published' do
     expect(page).to have_text("This website was last published on")
+  end
+end
+
+RSpec.shared_examples_for 'Location Page' do
+  it 'has a sea level visualization map' do
+    expect(page).to have_text("Visualize Sea Level Change in")
   end
 end
